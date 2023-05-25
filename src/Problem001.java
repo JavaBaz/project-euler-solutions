6e021f1547197f3783a669089b6e9b480fa4f3f1
JavaBaz <Bazgir.civil@gmail.com>
2023-05-25 22:15:05 +0330

    solution of problem 1 has been added.

---
+++ src/Problem001.java
@@ -0,0 +1,15 @@
+public class Problem001 {
+    public static void main(String[] args) {
+        Problem001 file = new Problem001();
+        file.run();
+    }
+
+    public void run() {
+        int sum = 0;
+        for (int i = 2; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        }
+        System.out.println("sum is : " + sum);
+    }
+}
