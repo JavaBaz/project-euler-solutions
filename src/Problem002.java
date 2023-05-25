6223f40578d8010e8e8d7bab6e847384b8f0115c
JavaBaz <Bazgir.civil@gmail.com>
2023-05-25 22:10:32 +0330

    solution of problem 2 has been added.

---
+++ src/Problem002.java
@@ -0,0 +1,29 @@
+public class Problem002 {
+
+    public static void main(String[] args) {
+        Problem002 file = new Problem002();
+        file.run();
+    }
+
+
+
+    public void run(){
+        int n1 = 1, n2 = 2, n3, i;
+        System.out.print(n1 + " " + n2);
+        i = 2;
+        n3 = 0;
+        long sum = 2;
+        while (n3 < 3_000_000) {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+            i++;
+            if (n3 % 2 != 0) {
+                sum += n3;
+            }
+        }
+        System.out.println();
+        System.out.println("sum is : " + sum);
+    }
+}
