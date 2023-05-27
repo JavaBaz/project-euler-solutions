c85e1b6298986e8a6d5b4f93d4a4cf54e1c9458c
JavaBaz <Bazgir.civil@gmail.com>
2023-05-27 18:19:26 +0330

    solution of problem 3 has been added.

---
+++ src/Problem003.java
@@ -0,0 +1,32 @@
+public class Problem003 {
+
+    public static void main(String[] args) {
+        Problem003 file = new Problem003();
+        Long n = 600851475143L;
+        file.run(n);
+    }
+
+
+    public void run(Long n) {
+        System.out.print("Prime factor(s) of " + n + " is/are :");
+        // Print the number of 2s that divide n
+        while (n % 2 == 0) {
+            System.out.print(2 + " ");
+            n /= 2;
+        }
+
+        // n must be odd at this point.  So we can
+        // skip one element (Note i = i +2)
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            // While i divides n, print i and divide n
+            while (n % i == 0) {
+                System.out.print(i + " ");
+                n /= i;
+            }
+        }
+
+        // This condition is to handle the case when
+        // n is a prime number greater than 2
+        if (n > 2) System.out.print(n + " ");
+    }
+}
