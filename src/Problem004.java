29d17ca8e90674186851dd25eff85e622d44e681
JavaBaz <Bazgir.civil@gmail.com>
2023-05-29 02:50:55 +0330

    Add solution of problem 4.

---
+++ src/Problem004.java
@@ -0,0 +1,32 @@
+public class Problem004 {
+
+    public static void main(String[] args) {
+        System.out.println(new Problem004().run());
+    }
+
+    public static boolean isPalindrome(String string) {
+        StringBuilder reversedString = new StringBuilder();
+        char tmpChar;
+        int lengthOfString = string.length();
+
+        for (int i = 0; i < lengthOfString; i++) {
+            tmpChar = string.charAt(i);
+            reversedString.insert(0, tmpChar);
+        }
+
+        return string.contentEquals(reversedString);
+    }
+
+
+    public String run() {
+        int Palindrome = -1;
+        for (int i = 100; i < 1000; i++) {
+            for (int j = 100; j < 1000; j++) {
+                int prod = i * j;
+                if (isPalindrome(String.valueOf(prod)) && prod > Palindrome)
+                    Palindrome = prod;
+            }
+        }
+        return Integer.toString(Palindrome);
+    }
+}
