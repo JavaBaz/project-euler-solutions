9f9695307df7faaf476934adcf96ca4fdac4eb0c
JavaBaz <Bazgir.civil@gmail.com>
2023-05-29 03:15:58 +0330

    Add solution of problem 5.

---
+++ src/Problem005.java
@@ -0,0 +1,26 @@
+public class Problem005 {
+    public static void main(String[] args) {
+        System.out.println(new Problem005().run());
+    }
+
+
+    private boolean isEvenlyDivisible(int number) {
+        boolean result = true;
+        for (int i = 2; i <= 20; i++) {
+            if (number % i != 0) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    private int run() {
+        int answer = 2520;
+        while (!isEvenlyDivisible(answer)){
+            answer++;
+        }
+        return answer;
+    }
+}
