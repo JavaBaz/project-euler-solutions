b1c92e037a6462bfca6fc4228402db2045567419
JavaBaz <Bazgir.civil@gmail.com>
2023-06-08 14:54:48 +0330

    Add run() method to RunApp.java.

--- src/ui/RunApp.java
+++ src/ui/RunApp.java
@@ -1,5 +1,34 @@
 package ui;
 
+import java.util.Scanner;
+
 public class RunApp {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void run() {
+
+        int choice;
+        do {
+            Printer.printMenu(Menu.STARTMENU);
+            Printer.printMessage("Enter your choice : ");
+
+            choice = scanner.nextInt();
+            System.out.println();
+
+            switch (choice) {
+                case 1:
+                    ShowProblems();
+
+                case 0:
+                    System.out.println("Exiting the program. Goodbye!");
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+            System.out.println();
+        } while (choice != 0);
+
+        scanner.close();
+    }
 
 }
