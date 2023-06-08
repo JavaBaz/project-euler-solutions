6f5348c3b4c788a16203521e3cc046df69e38940
JavaBaz <Bazgir.civil@gmail.com>
2023-06-08 14:32:03 +0330

    Add String STARTMENU to Menu.java.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,5 @@
+import ui.Menu;
+
 public class Main {
+
 }
--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,38 +1,8 @@
 package ui;
 
-import java.util.Scanner;
-
 public class Menu {
-    static Scanner scanner = new Scanner(System.in);
 
-
-    public static void run() {
-        int choice;
-        do {
-            System.out.println("================");
-            System.out.println("Options:");
-            System.out.println("1. Select a Problem");
-            System.out.println("0. Exit");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
-            System.out.println();
-
-            switch (choice) {
-                case 1:
-                    selectAProblem();
-
-
-
-                case 0:
-                    System.out.println("Exiting the program. Goodbye!");
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-
-            System.out.println();
-        } while (choice != 0);
-
-        scanner.close();
-    }
+    public static String[] STARTMENU =
+            {"[1] - Show Problems", "[2] - About", "[3] - Exit"};
 
 }
