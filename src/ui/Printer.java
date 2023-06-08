6b971247d6857bd8a12216d39d2a8f47059f7072
JavaBaz <Bazgir.civil@gmail.com>
2023-06-08 14:41:36 +0330

    Add printMenu method to Printer.java.

--- src/ui/Printer.java
+++ src/ui/Printer.java
@@ -1,4 +1,11 @@
 package ui;
 
+import java.util.stream.Stream;
+
 public class Printer {
+
+    public static void printMenu(String[] items) {
+        Stream.of(items).forEach(System.out::println);
+    }
+
 }
